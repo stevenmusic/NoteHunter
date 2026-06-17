5f8a938f1e3c7c18693611ef3d1e59f13e59530f
stevenmusic <sifan888494@gmail.com>
2026-06-17 10:33:19 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,13 @@
 const player = {
   name: "Steven",
   level: 1,
+  exp: 0,
+
+  atk: 10,
   hp: 100,
-  score: 0,
-  combo: 0
+  def: 5,
+
+  weapon: { name: "木劍", atk: 1 },
+  armor: { name: "布衣", hp: 10 },
+  shield: { name: "木盾", def: 2 }
 };
