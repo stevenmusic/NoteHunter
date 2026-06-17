8e78d97d870488e8e4191f5472d66647f153656e
stevenmusic <sifan888494@gmail.com>
2026-06-17 23:18:26 +0800

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,26 @@
+// questions.js
+const NOTE_QUESTIONS = {
+  // 🎼 高音譜號：C4 (中央C) 到 C6 (高音C)
+  treble: [
+    { id: "t_c4", answer: "C", name: "C4", img: "assets/notes/IMG_2528.png" },
+    { id: "t_d4", answer: "D", name: "D4", img: "assets/notes/IMG_2529.png" },
+    { id: "t_e4", answer: "E", name: "E4", img: "assets/notes/IMG_2530.png" },
+    { id: "t_f4", answer: "F", name: "F4", img: "assets/notes/IMG_2531.png" },
+    { id: "t_g4", answer: "G", name: "G4", img: "assets/notes/IMG_2532.png" },
+    { id: "t_a4", answer: "A", name: "A4", img: "assets/notes/IMG_2533.png" },
+    { id: "t_b4", answer: "B", name: "B4", img: "assets/notes/IMG_2534.png" },
+    { id: "t_c5", answer: "C", name: "C5", img: "assets/notes/IMG_2535.png" },
+    { id: "t_d5", answer: "D", name: "D5", img: "assets/notes/IMG_2536.png" },
+    { id: "t_e5", answer: "E", name: "E5", img: "assets/notes/IMG_2537.png" },
+    { id: "t_f5", answer: "F", name: "F5", img: "assets/notes/IMG_2538.png" },
+    { id: "t_g5", answer: "G", name: "G5", img: "assets/notes/IMG_2539.png" },
+    { id: "t_a5", answer: "A", name: "A5", img: "assets/notes/IMG_2540.png" },
+    { id: "t_b5", answer: "B", name: "B5", img: "assets/notes/IMG_2541.png" },
+    { id: "t_c6", answer: "C", name: "C6", img: "assets/notes/IMG_2542.png" }
+  ],
 
+  // 𝄢 低音譜號：C2 到 C4 (待填入你的低音照片檔名)
+  bass: [
+    // 等你把低音譜號的照片號碼告訴我，我們就把這邊一次補齊！
+  ]
+};
